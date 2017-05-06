3d6448909f4f71f0c8ba36fb3923334aceea8a9a
Hakonator <sk22007@bk.ru>
2017-05-06 23:47:46 +0500

    пустой проект

---
+++ IT-spec/app/src/main/java/ru/korobov/serg/it_spec/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.korobov.serg.it_spec;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
